f4f4d02648b5e5ec654b2950c34b24a605fd4ab6
Arun bala <arun.bala@yellow.ai>
2023-03-08 09:04:23 +0530

    Next config changes for font

--- next.config.js
+++ next.config.js
@@ -7,26 +7,10 @@ const nextConfig = {
   env: {
     API_KEY: process.env.REACT_APP_API_KEY,
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
+  experimental: {
+    fontLoaders: [
+      { loader: "next/font/google", options: { subsets: ["latin"] } },
+    ],
   },
 };
 
